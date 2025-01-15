d90176235a023fbb2bf8441517f6e29a4da216ac
ketexon <zane.a.s.clark@gmail.com>
2025-01-15 14:41:46 -0800

    removed accidental debug print to main

--- src/Main.cpp
+++ src/Main.cpp
@@ -3,6 +3,5 @@
 int main(){
 	std::cout << "Running " << KTest::TestCase::NumTests() << " tests" << std::endl;
 	KTest::TestCase::RunAll();
-	std::cout << "MSVC Version: " << _MSVC_LANG << std::endl;
 	return 0;
 }
